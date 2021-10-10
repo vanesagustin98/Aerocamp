83d00b997434570a2358ff829341a7e0df544c45
diulioz <diulioz.u@gmail.com>
2021-10-10 15:07:03 -0500

    David 2

--- Interfaces Aeropuerto/ProgramarVuelo/conexion_bd.py
+++ Interfaces Aeropuerto/ProgramarVuelo/conexion_bd.py
@@ -1,19 +1,10 @@
 import psycopg2
-class conexion:
-    def __init__(self, password):
-        print("Entró")
-        self.password = input("Contraseña: ")
-        self.conectar()
-    def conectar(self):
-            psycopg2.connect(
-                host        = "localhost" , 
-                database    = "aerocamp" ,
-                user        = "postgres" ,
-                password    = self.password
-                )
-            print("Conexión exitosa")
-        # cursor = conexion.cursor()
-        # cursor = self.conectar.cursor()
-        
 
-        # probando
+def conexion_aerocampbd():
+    conexion = psycopg2.connect(
+        host = "localhost" , 
+        database = "aerocamp" ,
+        user = "postgres" ,
+        password = "root1"
+        )
+    return conexion
